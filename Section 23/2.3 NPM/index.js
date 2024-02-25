85a068f4068199f53b71ca764c44f29e232892b0
ivanlong20 <ivan.kaalong@gmail.com>
2024-02-25 09:20:54 +0800

    push

--- Section 23/2.3 NPM/index.js
+++
@@ -1,7 +0,0 @@
-// var generateName = require("sillyname");
-// var sillyName = generateName();
-
-// console.log("YOur name is" + sillyName + ".");
-import hero from "superheroes";
-
-console.log(`I'm ${hero.random()}`);

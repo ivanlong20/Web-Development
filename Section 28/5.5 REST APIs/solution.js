9fb2c3c881691dbe406b0100dad40c76e772c8b1
Ivan Chan <82087413+ivanlong20@users.noreply.github.com>
2024-03-01 18:19:13 +0800

    section 29

--- Section 28/5.5 REST APIs/solution.js
+++ Section 28/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "63ab2f9c-bbda-4196-bde4-cca2e3beff7a";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };

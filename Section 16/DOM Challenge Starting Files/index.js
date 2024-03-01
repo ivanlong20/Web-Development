f1385142b173803d2779bee2ae7a8bda45f7e4be
Ivan Chan <82087413+ivanlong20@users.noreply.github.com>
2024-03-01 13:04:17 +0800

    push

--- Section 16/DOM Challenge Starting Files/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("ul").lastElementChild.innerHTML = "Ivan";
-document.querySelector("li a").style.color = "green";
-document.querySelector("button").style.backgroundColor = "yellow";
-document.querySelector("h1").classList.toggle("huge");
